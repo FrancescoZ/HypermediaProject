adf49a23a16a9db1bfd89fd11a70704adcbe18a2
Francesco Di Febbo <francesco.difebbo@gmail.com>
2017-06-03 17:56:15 +0200

    Changes in location's landing page

--- public/assets/js/location-landing.js
+++ public/assets/js/location-landing.js
@@ -1,8 +1,48 @@
+const locations = [
+  {"id": 1,
+   "name": "Buckingham Palace",
+   "address": "Westminster, Londra SW1A 1AA, Regno Unito",
+   "info": "",
+   "how_to": "",
+   "lat": 51.5285582,
+   "long": -0.2416991
+  },
+  {"id": 1,
+   "name": "Torre di Londra",
+   "address": "St Katharine's & Wapping, Londra EC3N 4AB, Regno Unito",
+   "info": "",
+   "how_to": "",
+   "lat": 51.5285582,
+   "long": -0.2416991
+  },
+  {"id": 1,
+   "name": "London Eye",
+   "address": "Lambeth, Londra SE1 7PB, Regno Unito",
+   "info": "",
+   "how_to": "",
+   "lat": 51.5285582,
+   "long": -0.2416991
+  }
+]
+
+
 function init() {
   $('#location-info').show();
   $('#button-info').addClass("active")
   $('#location-map').hide();
   $('#button-map').removeClass("active")
+
+  var location = locations["0"]
+  document.getElementById("title").innerHTML = location.name;
+  document.getElementById("address").innerHTML = location.address;
+  /*fetch(`/services?start=${start}&limit=${count}&sort=${sortby}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      clearTable();
+      data.map(addRow);
+    });*/
 }
 
 function clickMap() {
@@ -17,6 +57,10 @@ function clickInfo() {
   $('#button-info').addClass("active")
   $('#location-map').hide();
   $('#button-map').removeClass("active")
+}
+
+function updatePetsList() {
+
 }
 
 init();
