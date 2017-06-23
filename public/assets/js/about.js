d719c5779a185876d1c4be101b822986e1f699df
Marco Travaglini <marco1.travaglini@mail.polimi.it>
2017-06-23 13:56:41 +0200

    added db interaction in about page

--- public/assets/js/about.js
+++ public/assets/js/about.js
@@ -1,42 +1,41 @@
-const egabout = ["Our medical team", "History", "Curiosity"]
-const egwhere = ["Location &amp; info"]
-const egprojects = "Project"
-const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum nulla lacus, at vehicula nisi finibus ut. Phasellus et porta nisi. Praesent efficitur turpis dui, in fringilla magna accumsan commodo. Maecenas gravida venenatis lorem id tempor. Aenean ligula orci, ullamcorper eu finibus vel, cursus ac metus. Phasellus nisi nunc, imperdiet bibendum rutrum sed, semper nec odio. Maecenas ac dui blandit, porttitor turpis ut, consequat elit. Nunc congue, mi ut interdum aliquam, felis diam bibendum quam, non accumsan felis justo eu dui. Mauris maximus, neque quis suscipit rhoncus, elit velit accumsan elit, eget molestie nisl ex vitae justo. Maecenas quis sem sit amet nisi dictum ultrices. Praesent tempus fermentum malesuada. Mauris tristique ut sapien et aliquet. Mauris tortor ante, pellentesque eu tristique vitae, aliquet at orci. Praesent massa purus, hendrerit vitae pretium nec, consequat hendrerit sem. Suspendisse potenti. Phasellus malesuada mattis metus et semper."
+const params = urlParams()
+const about_query = '/whoweare/'
 
 
 function init() {
   $('#about-panel').show();
   $('#about-btn').addClass("active")
-  $('#where-panel').hide();
-  $('#where-btn').removeClass("active")
+  $('#history-panel').hide();
+  $('#history-btn').removeClass("active")
   $('#projects-panel').hide();
   $('#projects-btn').removeClass("active")
-
-  for (var i in egabout) {
-    if (i != 0) {
-      $("#about-panel").append(`<br>`)
-    }
-    $("#about-panel").append(getParagraph(egabout[i], description))
-  }
-
-  for (var i in egwhere) {
-    if (i != 0) {
-      $("#where-panel").append(`<br>`)
-    }
-    $("#where-panel").append(getParagraph(egwhere[i], description))
-  }
-
-  for (i = 0; i < 4; i++) {
-    if (i != 0) {
-      $("#projects-panel").append(`<br>`)
-    }
-    $("#projects-panel").append(getParagraph(egprojects + ` ${i + 1}`, description))
+  
+  fetchContent(about_query)
+}
+  
+  function initContent(query, item, index) {
+  if (query.includes(about_query)) {
+    initWhoWeAre(item, index)
+  } else {
+    console.log("Error: " + query)
   }
 }
-
-function getParagraph(title, text) {
-  return `<h2>${title}</h2><p>${text}</p>`
-}
+  
+  function initWhoWeAre(item, index) {
+    if(item.tab == "About") {
+      if (item.id != 1) {
+        $("#about-panel").append(`<br>`)
+      }
+      $("#about-panel").append(`<h2>${item.title}</h2><p>${item.text}</p>`)
+    } else if(item.tab == "History") {
+      $("#history-panel").append(`<p>${item.text}</p>`)
+    } else if(item.tab == "Project") {
+      if (item.id != 4) {
+        $("#projects-panel").append(`<br>`)
+      }
+      $("#projects-panel").append(`<h2>${item.title}</h2><p>${item.text}</p>`)
+    }
+  }
 
 init()
 
@@ -45,8 +44,17 @@ init()
 function clickAbout() {
   $('#about-panel').show();
   $('#about-btn').addClass("active")
-  $('#where-panel').hide();
-  $('#where-btn').removeClass("active")
+  $('#history-panel').hide();
+  $('#history-btn').removeClass("active")
+  $('#projects-panel').hide();
+  $('#projects-btn').removeClass("active")
+}
+
+function clickHistory() {
+  $('#about-panel').hide();
+  $('#about-btn').removeClass("active")
+  $('#history-panel').show();
+  $('#history-btn').addClass("active")
   $('#projects-panel').hide();
   $('#projects-btn').removeClass("active")
 }
@@ -54,11 +62,12 @@ function clickAbout() {
 function clickProjects() {
   $('#about-panel').hide();
   $('#about-btn').removeClass("active")
-  $('#where-panel').hide();
-  $('#where-btn').removeClass("active")
+  $('#history-panel').hide();
+  $('#history-btn').removeClass("active")
   $('#projects-panel').show();
   $('#projects-btn').addClass("active")
 }
+
 
 function clickContactUs() {
   document.location.href = `/contact-us.html?`
