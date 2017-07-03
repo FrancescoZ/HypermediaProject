32a1a6a0f717f025905b5eacb225aa1bb30b61e8
Francesco Zanoli <francesco.zanoli@gmail.com>
2017-07-03 12:59:42 +0200

    Added doc in index.js and correct utitlities

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 /*****************************************************  INDEX  ********************************************************/
+const utils=require("./other/utilities.js");
 
+//Module to manage the request to the server
 const express = require("express");
 const app = express();
+
+//Parse incoming request bodies in a middleware before your handlers, available under the req.body property
 const bodyParser = require("body-parser");
+
+//Allow the override of the primaly function of express as the management of the error
 const methodOverride = require('method-override');
+
+//Provides information and control over the node js process
 const process = require("process");
 
+//Read the environement variable to initializate the process
 let serverPort = process.env.PORT || 5000;
 
+//Set the static file to distribute, this allow to not specifically answer to the get request concerning the html pages
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/public/pages"));
 
+//Set the middleware to parse Json request (containing content-type:json)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(methodOverride());
-app.use(function (err, req, res, next) {
-  console.log(err);
-  res.send(404);
-});
 
+//set the non-stardard error management
+app.use(methodOverride());
+app.use(utils.errorManager);
+//set the port from the env variable
 app.set("port", serverPort);
 
-/* Register REST entry point */
+/* Register REST API entry point */
 const news = require("./other/news.js")(app);
 const com = require("./other/comunication.js")(app);
 const doctors = require("./other/doctors.js")(app);
@@ -32,7 +42,7 @@ const services = require("./other/services.js")(app);
 const areas = require("./other/areas.js")(app);
 const whowe = require("./other/whoweare.js")(app);
 
-/* Call the different submodule, and crete the connection with the database */
+/* Init all the API module, it include the initialisation of the db*/
 function init() {
   areas.init();
   doctors.init();
--- other/utilities.js
+++ other/utilities.js
@@ -134,7 +134,7 @@ module.exports = {
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        return console.log(error);
+        return this.consoleError(error);
       }
       console.log('Message %s sent: %s', info.messageId, info.response);
     })
